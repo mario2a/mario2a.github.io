8e74ce37b2d4b7fe6f47e839596f86e91a076cd6
mario2a <150332530+mario2a@users.noreply.github.com>
2023-11-09 10:33:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://simple-proxy.marioserra.workers.dev",
-  VITE_TMDB_READ_API_KEY: "c0fbf1278e48a336841be386f5cea1ca"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGZiZjEyNzhlNDhhMzM2ODQxYmUzODZmNWNlYTFjYSIsInN1YiI6IjY1NGM5ZGJiNDFhNTYxMzM2OTNjY2U2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q3OzSgy4jhTURc3hy8rhkcnAzQXre7vbs_yAxfVTPcg"
 };
