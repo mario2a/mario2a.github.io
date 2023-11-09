99f86ae2a5a4afecb71779cb0f1b418212854921
mario2a <150332530+mario2a@users.noreply.github.com>
2023-11-09 10:06:47 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.marioserra.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "c0fbf1278e48a336841be386f5cea1ca"
+};
